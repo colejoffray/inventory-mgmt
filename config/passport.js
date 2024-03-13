a8d2dae9176a0a6bf78bde60f3075678a0cf4efa
Cole Joffray <colejoff@gmail.com>
2024-03-13 15:21:54 -0400

    redirect uri

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ module.exports = function(passport){
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "https://stockify-management.xyz/auth/google/callback"
+        callbackURL: "https://www.stockify-management.xyz/auth/google/callback"
       },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
